e9e888ba4a333407cf9b76e6d62b890e786677a5
Raul Laprida <raul.laprida@gmail.com>
2019-12-23 18:09:26 -0300

    Initial Commit - Still under development, it compiles but functionalitity is not finished

--- src/modules/nodesConf.ts
+++ src/modules/nodesConf.ts
@@ -1,43 +1,58 @@
 // Peer generator
 
-import * as yargs from 'yargs'
+import * as yargs from "yargs";
 
-import FsStore from 'datastore-fs'
-import { createNode, createNodeFromJSON } from './createNode'
-
-const datastore = new FsStore('./node-keystore')
+import LevelStore from "datastore-level"; //Web-Browser compatible store
+import { createNode, createNodeFromJSON } from "./createNode";
+import Multiaddr from "multiaddr";
 
 const myArgs = yargs
-  .option('socketport', {
-    type: 'number',
+  .option("socketport", {
+    type: "number",
     default: 0
   })
-  .default('chatClient', false)
-  .default('host', '127.0.0.1')
-  .default('port', '9090')
-  .option('webrtc', {
-    type: 'boolean',
+  .default("chatClient", false)
+  .default("host", "127.0.0.1")
+  .default("port", "9090")
+  .option("webrtc", {
+    type: "boolean",
     default: false
   })
-  .default('keyname', 'defaultkeyname')
-  .default('keytype', 'secp256k1')
-  .option('bootNodeAddr', {
-    type: 'string',
-    description: 'Address of a bootnode to connect to'
+  .default("keyname", "defaultkeyname")
+  .default("keytype", "secp256k1")
+  .option("bootNodeAddr", {
+    type: "string",
+    description: "Address of a bootnode to connect to"
   })
-  .default('key', true)
-  .default('keystore', '')
-  .default('automated', false)
-  .default('nodes', 10) // ONlY for test scenario
-  .default('algorithm', 'DEFAULT')
-  .default('ofuscate', true).argv
+  .default("createKey", true)
+  .default("keystore", "./node-keystore")
+  .default("automated", false)
+  .default("nodes", 10) // ONlY for test scenario
+  .default("algorithm", "DEFAULT")
+  .default("ofuscate", true)
+  .default("passphrase", "pass").argv;
 
-const generateKey = myArgs.key
+const datastore = new LevelStore("./node-keystore", {
+  db: require("level")
+});
 
-exports.createPeer = (keyname: string, callback: () => void) => {
-  myArgs.keystore !== ''
-    ? createNodeFromJSON(keyname, callback)
-    : createNode(keyname, callback)
+function addWebRTCMultiaddress(peerInfo: any): void {
+  const addr = new Multiaddr(
+    "/ip4/" +
+      myArgs.host +
+      "/tcp/" +
+      myArgs.port +
+      "/wss/p2p-webrtc-star/ipfs/" +
+      peerInfo.id.toB58String()
+  );
+  peerInfo.multiaddrs.add(addr);
 }
 
-export { myArgs, generateKey, datastore }
+function addSocketMultiaddress(peerInfo: any): void {
+  const addr = new Multiaddr(
+    "/ip4/" + myArgs.host + "/tcp/" + myArgs.socketport + "/wss"
+  );
+  peerInfo.multiaddrs.add(addr);
+}
+
+export { addSocketMultiaddress, addWebRTCMultiaddress, myArgs, datastore };
