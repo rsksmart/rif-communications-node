155795127fa279dec99d274f4249e43ed59e85b0
Raul Laprida <raul.laprida@gmail.com>
2019-12-23 18:09:26 -0300

    feat: initial commit - it compiles but functionalitity is not finished

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,60 @@
 // Main process
-import * as async from 'async'
-import { createNode, createNodeFromJSON } from './modules/createNode'
-import cryptoUtil from 'libp2p-crypto'
-import Multiaddr from 'multiaddr'
-import multihashingAsync from 'multihashing-async'
-import { myArgs } from './modules/nodesConf'
-import { CommandLineChat } from './modules/chatClient'
-import logger from './logger'
+import * as async from "async";
+import { createNode, createNodeFromJSON } from "./modules/createNode";
+import cryptoUtil from "libp2p-crypto";
+import Multiaddr from "multiaddr";
+import multihashingAsync from "multihashing-async";
+import { myArgs } from "./modules/nodesConf";
+import { CommandLineChat } from "./modules/chatClient";
+import logger from "./logger";
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-)
+process.on("unhandledRejection", (reason, p) =>
+  logger.error("Unhandled Rejection at: Promise ", p, reason)
+);
 
-const keyname = myArgs.keyname
-const keystore = myArgs.keystore
+const keyname = myArgs.keyname;
 
-function mainProcess () {
+function mainProcess() {
   async.waterfall(
     [
       (cb: () => void) => {
-        if (keystore !== '') {
-          return createNodeFromJSON(keystore, cb)
-        } else {
-          return createNode(keyname, cb)
-        }
+        myArgs.createKey
+          ? createNode(keyname, cb)
+          : createNodeFromJSON(keyname, cb);
       }
     ],
     (err: Error | null | undefined, node: any) => {
-      if (err) throw err
+      if (err) throw err;
 
-      logger.info('---- YOUR NODE INFORMATION ----')
-      logger.info('ID: %s', node.peerInfo.id._idB58String)
-      logger.info('ID length: %s', node.peerInfo.id.id.length)
-      logger.info('Multiaddresses:')
+      logger.info("---- YOUR NODE INFORMATION ----");
+      logger.info("ID: %s", node.peerInfo.id._idB58String);
+      logger.info("ID length: %s", node.peerInfo.id.id.length);
+      logger.info("Multiaddresses:");
       node.peerInfo.multiaddrs.forEach((ma: { toString: () => string }) =>
         logger.info(ma.toString())
-      )
+      );
       multihashingAsync.digest(
         node.peerInfo.id.id,
-        'sha2-256',
+        "sha2-256",
         (err: Error, dhtId: any) => {
-          if (err) throw err
+          if (err) throw err;
 
           logger.debug(
-            'Internal DHT ID: %s',
+            "Internal DHT ID: %s",
             multihashingAsync.multihash.toB58String(dhtId)
-          )
-          logger.debug('ID length: %s', dhtId.length)
-          logger.debug(dhtId)
+          );
+          logger.debug("ID length: %s", dhtId.length);
+          logger.debug(dhtId);
           logger.debug(
-            'Internal DHT ID is displayed for debug pursposes, never reference by this ID'
-          )
-          logger.info('---------------------------')
-          logger.info('PUBLIC KEY')
+            "Internal DHT ID is displayed for debug pursposes, never reference by this ID"
+          );
+          logger.info("---------------------------");
+          logger.info("PUBLIC KEY");
           logger.info(
             cryptoUtil.keys
-              .marshalPublicKey(node.peerInfo.id._pubKey, 'secp256k1')
-              .toString('base64')
-          )
+              .marshalPublicKey(node.peerInfo.id._pubKey, "secp256k1")
+              .toString("base64")
+          );
 
           if (myArgs.bootNodeAddr) {
             async.parallel(
@@ -69,21 +66,21 @@ function mainProcess () {
               ],
               (err: Error | null | undefined) => {
                 if (err) {
-                  throw err
+                  throw err;
                 }
-                logger.info('Connection Successful')
+                logger.info("Connection Successful");
 
                 if (myArgs.chatClient) {
-                  const chatClient: CommandLineChat = new CommandLineChat(node)
-                  chatClient.init()
+                  const chatClient: CommandLineChat = new CommandLineChat(node);
+                  chatClient.init();
                 }
               }
-            )
+            );
           }
         }
-      )
+      );
     }
-  )
+  );
 }
 
-mainProcess()
+mainProcess();
